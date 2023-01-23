50051c1344bbecaa5f0d0898b50ec9d36db8c96b
Rohit <rohit2k02@gmail.com>
2023-01-23 18:43:43 +0530

    Code Update

--- TemperatureFinal.py
+++ TemperatureFinal.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 import joblib
 
-rawdata=pd.read_csv("DailyTemperatures.csv")
-X=rawdata.drop(columns=['AvgTemperature','Region','Country','City'])
-y=rawdata['AvgTemperature']
+rawdata = pd.read_csv("DailyTemperatures.csv")
+X = rawdata.drop(columns=['AvgTemperature', 'Region', 'Country', 'City'])
+y = rawdata['AvgTemperature']
 
-model=DecisionTreeRegressor()
-model.fit(X,y)
-joblib.dump(model,'TemperatureMLModel.joblib')
+model = DecisionTreeRegressor()
+model.fit(X, y)
+joblib.dump(model, 'TemperatureMLModel.joblib')
